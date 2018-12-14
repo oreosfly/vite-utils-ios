820f4a286b110a36fe2e4704aa3dc8affd57fff0
Stone <stone.hsia@gmail.com>
2018-12-14 18:17:31 +0800

    remove ViteUtilsInitializer

--- ViteUtils/Classes/VitePodLanguageBundle.swift
+++ ViteUtils/Classes/VitePodLanguageBundle.swift
@@ -1,5 +1,5 @@
 //
-//  LocalizationService.swift
+//  VitePodLanguageBundle.swift
 //  Pods
 //
 //  Created by Stone on 2018/12/5.
@@ -11,19 +11,24 @@ extension Notification.Name {
     public static let localizedStringChanged = NSNotification.Name(rawValue: "net.vite.localized.string.changed")
 }
 
-public class ViteUtilsLocalizationService: NSObject {
+open class VitePodLocalizationService: NSObject {
 
-    static let  sharedInstance = ViteUtilsLocalizationService()
-    @objc public static func ocSharedInstance() -> ViteUtilsLocalizationService {
+    static public let  sharedInstance = VitePodLocalizationService()
+    @objc public static func ocSharedInstance() -> VitePodLocalizationService {
         return sharedInstance
     }
 
     fileprivate var currentLanguage: ViteLanguage = .base
     fileprivate var cacheTextDic: [String: String] = [:]
 
+    fileprivate var bundleName: String = ""
+
+    @objc public func setBundleName(_ bundleName: String) {
+        self.bundleName = bundleName
+    }
+
     private override init() {
         super.init()
-        object_setClass(Bundle(for: ViteUtilsLanguageBundle.self), ViteUtilsLanguageBundle.self)
         NotificationCenter.default.addObserver(forName: .localizedStringChanged, object: nil, queue: nil) { [weak self] notification in
             guard let `self` = self else { return }
             guard let userInfo = notification.userInfo,
@@ -32,7 +37,7 @@ public class ViteUtilsLocalizationService: NSObject {
                     return
             }
 
-            let prefix = "ViteUtils."
+            let prefix = "\(self.bundleName)."
             let dic = NSMutableDictionary()
             for (key, value) in cacheTextDic where key.hasPrefix(prefix) {
                 dic[(key as NSString).substring(from: prefix.count) as String] = value
@@ -43,21 +48,26 @@ public class ViteUtilsLocalizationService: NSObject {
     }
 }
 
-private class ViteUtilsLanguageBundle: Bundle {
+open class VitePodLanguageBundle: Bundle {
 
-    override func localizedString(forKey key: String, value: String?, table tableName: String?) -> String {
+    open class func podLocalizationServiceClass() -> AnyClass {
+        return VitePodLocalizationService.self
+    }
 
-        if let ret = ViteUtilsLocalizationService.sharedInstance.cacheTextDic[key] {
+    override open func localizedString(forKey key: String, value: String?, table tableName: String?) -> String {
+
+        let serviceSharedInstance = (type(of: self).podLocalizationServiceClass() as! VitePodLocalizationService.Type).sharedInstance
+        if let ret = serviceSharedInstance.cacheTextDic[key] {
             return ret
         }
 
-        var bundle = Bundle(for: ViteUtilsLanguageBundle.classForCoder())
-        if let resourcePath = bundle.path(forResource: "ViteUtils", ofType: "bundle"),
+        var bundle = Bundle(for: type(of: self).self)
+        if let resourcePath = bundle.path(forResource: serviceSharedInstance.bundleName, ofType: "bundle"),
             let resourcesBundle = Bundle(path: resourcePath) {
             bundle = resourcesBundle
         }
 
-        if let path = bundle.path(forResource: ViteUtilsLocalizationService.sharedInstance.currentLanguage.rawValue, ofType: "lproj"),
+        if let path = bundle.path(forResource: serviceSharedInstance.currentLanguage.rawValue, ofType: "lproj"),
             let bundle = Bundle(path: path) {
             return bundle.localizedString(forKey: key, value: value, table: tableName)
         }
